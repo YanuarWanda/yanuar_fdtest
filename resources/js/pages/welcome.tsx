29808c673e88553057f72056833875108634d82e
Yanuar Wanda <yanuar.wanda2@gmail.com>
2025-08-03 17:14:15 +0700

    fix: make filters in landing page alligned vertically

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -242,7 +242,7 @@ export default function Welcome({ auth, books, filters, authors }: WelcomePagePr
                                     />
                                 </div>
 
-                                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                                <div className="grid grid-cols-1 items-end gap-4 sm:grid-cols-2 lg:grid-cols-4">
                                     <Select value={getAuthorDisplayValue()} onValueChange={handleAuthorFilter}>
                                         <SelectTrigger className="w-full">
                                             <SelectValue placeholder="All Authors" />
